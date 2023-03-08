2ec809ab242b8fffd64902dba252ea2bc5599575
Ruben <rubenmontes330@gmail.com>
2023-03-08 19:57:58 +0000

    Componente Button y refactor

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
 import styled from 'styled-components'
 import { Formik, Form } from 'formik';
-import Input from './components/Input'
-const Container=styled.div`
-  display: flex;
-  justify-content:center;
-  height: 100%;
-  align-items: center;
-`
-const Section= styled.section`
-  background-color: #eee;
-  border-top: solid 2px palevioletred;
-  padding: 20px 25px;
-  width:500px;
-  box-shadow: 0px 2px 3px rgb(0,0,0,0.3);
-`
+import Input from './components/Input';
+import Button from './components/Button'
+import Container from './components/Container';
+import Section from './components/Section';
 
 function App() {
   const handleSubmit= ()=>{}
@@ -34,7 +24,7 @@ function App() {
           <Input name='contribution' label='Contribución anual'></Input>
           <Input name='years' label='Años'></Input>
           <Input name='rate' label='Interes estimado'></Input>
-
+          <Button> Calcular</Button>
           </Form>
         </Formik>
       </Section>
